68a557ca741f356e5069fffa322db09973a8cb92
Safah64 <safah7gamer99@gmail.com>
2026-03-02 12:22:25 +0100

    jump

--- Game_Jam_2/Assets/Scripts/Player movement.cs
+++ Game_Jam_2/Assets/Scripts/Player movement.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-   [SerializeField] private float speed = 5f;
-   [SerializeField] KeyCode right = KeyCode.D;
-   [SerializeField] KeyCode left = KeyCode.A;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] KeyCode right = KeyCode.D;
+    [SerializeField] KeyCode left = KeyCode.A;
+    [SerializeField] KeyCode jump = KeyCode.Space;
     void Start()
     {
         print(speed);
+        print(jumpForce);
     }
 
     // Update is called once per frame
@@ -20,6 +23,10 @@ public class Playermovement : MonoBehaviour
         if (Input.GetKey(left))
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(jump))
+        {
+            transform.Translate(Vector3.up * jumpForce * Time.deltaTime);
         }
     }
 }
