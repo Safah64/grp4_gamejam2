b832a4cd055347c836212bd64bd9d64ab8fa411b
Safah64 <safah7gamer99@gmail.com>
2026-03-02 12:50:38 +0100

    jump fix

--- Game_Jam_2/Assets/Scripts/Player movement.cs
+++ Game_Jam_2/Assets/Scripts/Player movement.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 public class Playermovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    [SerializeField] private float jumpForce = 5f;
     [SerializeField] KeyCode right = KeyCode.D;
     [SerializeField] KeyCode left = KeyCode.A;
-    [SerializeField] KeyCode jump = KeyCode.Space;
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+
     void Start()
     {
         print(speed);
         print(jumpForce);
+
+        rb= GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,9 +35,22 @@ public class Playermovement : MonoBehaviour
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
-        if (Input.GetKey(jump))
+       
+
+        // Check if grounded
+        isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundDistance, groundLayer);
+
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
         {
-            transform.Translate(Vector3.up * jumpForce * Time.deltaTime);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+
+
     }
+
+
+
+
+
+   
 }
