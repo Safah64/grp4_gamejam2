247c16030c97c94d78dd497d10bea9fa8f005c1b
Safah64 <safah7gamer99@gmail.com>
2026-03-04 10:41:09 +0100

    enemis

--- Game_Jam_2/Assets/Scripts/enemies.cs
+++ Game_Jam_2/Assets/Scripts/enemies.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 public class enemies : MonoBehaviour
 {
     public int health = 100;
-   
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float force;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 direction = player.transform.position - transform.position;
+        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * force;
+    }
 
 
 
