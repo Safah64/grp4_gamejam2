22fac88dd098cd49600cc2cabc1460653bcf6bd0
Safah64 <safah7gamer99@gmail.com>
2026-03-06 10:51:06 +0100

    level 1 more

--- Game_Jam_2/Assets/Scripts/portolScript.cs
+++ Game_Jam_2/Assets/Scripts/portolScript.cs
@@ -3,13 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    public string Level2;
+    public string nextLevelName;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(Level2);
+            SceneManager.LoadScene(nextLevelName);
         }
     }
 }
